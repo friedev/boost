54c9f3df3d26cce42aeded2c0766de9cc103914c
Aaron Friesen <maugrift@maugrift.com>
2022-03-28 14:34:35 -0500

    Update bot.sh to be path independent
    It also accepts the token path as an argument now.

--- bot.sh
+++ bot.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-# Runs the Discord bot using the token stored in token.txt
-python3 -m boost-game.bot "$(cat token.txt)"
+# Runs the Discord bot using the token read from a file (default: token.txt)
+if [ "$1" ]; then
+	token_path="$(realpath "$1")"
+else
+	token_path=token.txt
+fi
+cd "$(dirname "$0")" || exit 1
+python3 -m boost_game.bot "$(cat "$token_path")"
